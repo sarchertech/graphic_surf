511701e8a616cd3304667c1f69b626391fdbb3e1
Seth Brown <learc83@gmail.com>
2008-12-03 10:41:04 -0500

    modified deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,5 @@
 set :application, "graphicsurf.com"
 set :user, "surfer"
-set :repository,  "git@github.com:learc83/graphic_surf.git"
 
 # If you aren't deploying to /u/apps/#{application} on the target
 # servers (which is the default), you can specify the actual location
@@ -15,8 +14,22 @@ set :deploy_to, "/home/surfer/public_html/#{application}"
 # your SCM below:
 # set :scm, :subversion
 set :scm, "git"
+set :repository,  "git@github.com:learc83/graphic_surf.git"
+set :branch, "master"
 set :deploy_via, :remote_cache
 
 role :app, application
 role :web, application
 role :db,  application, :primary => true
+
+namespace :deploy do
+  desc "Restarting mod_rails with restart.txt"
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "touch #{release_path}/tmp/restart.txt"
+  end
+ 
+  [:start, :stop].each do |t|
+    desc "#{t} task is a no-op with mod_rails"
+    task t, :roles => :app do ; end
+  end
+end
