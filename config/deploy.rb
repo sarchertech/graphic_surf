203f8677ad3ed011d6c3f1cde98cbd0fbc81d210
Seth Brown <learc83@gmail.com>
2008-12-03 10:21:32 -0500

    modified deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,15 +1,24 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+set :application, "graphic_surf"
+set :user, "surfer"
+set :repository,  "git@github.com:learc83/graphic_surf.git"
 
 # If you aren't deploying to /u/apps/#{application} on the target
 # servers (which is the default), you can specify the actual location
 # via the :deploy_to variable:
 # set :deploy_to, "/var/www/#{application}"
 
+set :port, 31000
+
+set :deploy_to, "/home/surfer/public_html/#{application}"
+
 # If you aren't using Subversion to manage your source code, specify
 # your SCM below:
 # set :scm, :subversion
+set :scm, "git"
+set :deploy_via, :remote_cache
 
-role :app, "your app-server here"
-role :web, "your web-server here"
-role :db,  "your db-server here", :primary => true
+set :location, "graphicsurf.com"
+
+role :app, location
+role :web, location
+role :db,  location, :primary => true
