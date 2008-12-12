b4c2c6974e1cb912bc087af2466bab0dbe611510
Seth Brown <learc83@gmail.com>
2008-12-12 18:31:16 -0500

    changed form validation and diver png

--- public/javascripts/contact_validation.js
+++ public/javascripts/contact_validation.js
@@ -2,5 +2,5 @@ var field1 = new LiveValidation( 'contact_name', {onlyOnSubmit: true } );
 field1.add( Validate.Presence );
 var field2 = new LiveValidation( 'contact_email', {onlyOnSubmit: true } );
 field2.add( Validate.Email );
-var field3 = new LiveValidation( 'contact_phone', {onlyOnSubmit: true } );
-field3.add( Validate.Numericality );
+var field3 = new LiveValidation( 'contact_phone', {onlyOnSubmit: true} );
+field3.add( Validate.Length, { minimum: 10, tooShortMessage: "Must be at least 10 numbers!" } );
